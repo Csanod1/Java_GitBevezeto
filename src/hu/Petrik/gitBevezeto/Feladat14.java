c09c10761ce0d0aacf3eb3b10c74baa8f5d831bf
Kormos Csanád <csanadsuli19@gmail.com>
2022-09-13 16:14:40 +0200

    Feladat befejezése

--- src/hu/Petrik/gitBevezeto/Feladat14.java
+++ src/hu/Petrik/gitBevezeto/Feladat14.java
@@ -1,4 +1,5 @@
 package hu.Petrik.gitBevezeto;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Feladat14 {
@@ -7,6 +8,47 @@ public class Feladat14 {
         System.out.println("Kérem adjon meg egy szöveget");
 
         String szoveg = sc.nextLine();
+        String menuPont = "";
+         while(!menuPont.equals("f")){
+             System.out.println("Adja meg, hogy mit szeretne csinálni a szöveggel:");
+             System.out.println("\ta - nagybetűssé alakítás");
+             System.out.println("\tb - Kisbetűssé alakítani");
+             System.out.println("\tc - lekérdezni a hosszát");
+             System.out.println("\td - Összehasonlítani a másik szöveggel");
+             System.out.println("\te - A szöveg egy részét kiíratni");
+             System.out.println("\tf - Kilépés");
+             System.out.println("Adja meg a menüpontot: ");
+             menuPont = sc.nextLine();
+             switch (menuPont){
+                 case "a":
+                     System.out.println(szoveg.toUpperCase());
+                     break;
+                 case "b":
+                     System.out.println(szoveg.toLowerCase());
+                     break;
+                 case "c":
+                     System.out.printf("A szöveg hossza %d karakter\n", szoveg.length());
+                     break;
+                 case "d":
+                     System.out.println("Kérem adja meg a másik szöveget: ");
+                     String szoveg2 = sc.nextLine();
+                     int osszehasonlitas = szoveg.compareToIgnoreCase(szoveg2);
+                     if (osszehasonlitas < 0){
+                         System.out.println("Az első szöveg előrébb van az ABC-ben");
+                     }else if (osszehasonlitas > 0){
+                         System.out.println("A második szöveg előrébb van az ABC-ben");
+                     }else{
+                         System.out.println("A két szöveg ugyan az");
+                     }
+                     break;
+                 case "e":
+                     break;
+                 case "f":
+                     break;
+                 default:
 
+                     break;
+             }
+         }
     }
 }
