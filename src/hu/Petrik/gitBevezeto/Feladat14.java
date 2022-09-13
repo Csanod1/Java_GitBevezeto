c1149c7203aaa16023762437090451b675995ddc
Kormos Csanád <csanadsuli19@gmail.com>
2022-09-13 15:36:39 +0200

    Szöveg bekérése

---
+++ src/hu/Petrik/gitBevezeto/Feladat14.java
@@ -0,0 +1,12 @@
+package hu.Petrik.gitBevezeto;
+import java.util.Scanner;
+
+public class Feladat14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kérem adjon meg egy szöveget");
+
+        String szoveg = sc.nextLine();
+
+    }
+}
